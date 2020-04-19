da7794314aefbd399f4ae9206abce7e2ba072268
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-04-18 21:25:59 -0300

    Update root to check if an error occur

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
@@ -9,10 +12,16 @@ var (
 		Use:          "navio",
 		Short:        "Atenção tripulação, os containers estão surgindo!",
 		SilenceUsage: true,
+		// Run: func(cmd *cobra.Command, args []string) {
+		// 	fmt.Println("Root cmd")
+		// },
 	}
 )
 
 // Execute executes the root command.
-func Execute() error {
-	return rootCmd.Execute()
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
