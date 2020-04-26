8984a487ab68e535dd1a9d2020e7359f099ed5ce
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-04-26 14:33:15 -0300

    Update the container cmd logs

--- cmd/container.go
+++ cmd/container.go
@@ -2,10 +2,14 @@ package cmd
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/viniciusbds/navio/src/container"
+	"github.com/viniciusbds/navio/src/logger"
 )
+
+var l = logger.New(time.Kitchen, true)
 
 func init() {
 	rootCmd.AddCommand(createContainer())
@@ -15,14 +19,14 @@ func createContainer() *cobra.Command {
 	return &cobra.Command{
 		Use: "run",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println("Args: ", args)
-
 			// navio run IMAGE COMMAND PARAMS...
 			image := args[0]
 			command := args[1]
 			params := args[2:]
 
-			args = append([]string{"runa", image, command}, params...)
+			l.Log("INFO", fmt.Sprintf("Image: %s, Command: %s, Params: %v", image, command, params))
+
+			args = append([]string{"run", image, command}, params...)
 
 			container.CreateContainer(args)
 
