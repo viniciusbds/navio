207c2a2e1c7f7311ad41fae7ccfbc283d53c576c
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-04-26 14:01:36 -0300

    Update the structure code of the package container

--- src/container/container.go
+++ src/container/container.go
@@ -8,31 +8,80 @@ import (
 	"path/filepath"
 	"strconv"
 	"syscall"
+	"time"
+
+	"github.com/docker/docker/pkg/reexec"
+	"github.com/viniciusbds/navio/src/logger"
+	"github.com/viniciusbds/navio/src/util"
 )
 
-// docker         run image <cmd> <params>
-// go run main.go run image <cmd> <params>
-// navio container image <cmd> <params>
+var l = logger.New(time.RFC3339, true)
 
-// CreateContainer creates a container
+func init() {
+	reexec.Register("child", child)
+	if reexec.Init() {
+		os.Exit(0)
+	}
+}
+
+func child() {
+	fmt.Printf("\n>> namespace setup code goes here <<\n\n")
+	childRun(os.Args[1], os.Args[2], os.Args[3:])
+}
+
+func childRun(image string, command string, params []string) {
+
+	configureCgroups()
+
+	syscall.Sethostname([]byte("container"))
+
+	syscall.Chroot("../images/ubuntu")
+	os.Chdir("/")
+
+	util.Must(mountProc(""))
+
+	cmd := exec.Command(command, params...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	util.Must(cmd.Run())
+	util.Must(unmountProc(""))
+}
+
+// CreateContainer creates a container. Receive as argument: ["run", <image-name>, <command>, <params> ]
 // Params: args
 //          args[0] (run, child)
 //          args[1] command
 //          args[2:]... params
 func CreateContainer(args []string) {
-	//fmt.Println(args)
-	switch args[0] {
-	case "run":
-		run(args[1], args[2:])
-	case "child":
-		child(args[1], args[2:])
-	default:
-		panic("bad command")
+
+	if args[0] != "run" {
+		l.Log("error", "Bad command")
+		os.Exit(1)
 	}
+
+	image, command, params := args[1], args[2], args[3:]
+
+	switch image {
+	case "ubuntu":
+		// do ...
+	case "arch":
+		// do ...
+	case "alpine":
+		// do ...
+	default:
+		// do ...
+
+	}
+
+	run(image, command, params)
 }
 
-func run(command string, params []string) {
-	cmd := exec.Command("./navio", append([]string{"container", "child", command}, params...)...)
+func run(image string, command string, params []string) {
+	//cmd := exec.Command("/proc/self/exe", append([]string{"child", command}, params...)...)
+	cmd := reexec.Command(append([]string{"child", image, command}, params...)...)
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
@@ -42,29 +91,7 @@ func run(command string, params []string) {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	fmt.Println(cmd)
-	must(cmd.Run())
-}
-
-func child(command string, params []string) {
-	//fmt.Printf("Running child %v as %d\n", os.Args[2:], os.Getpid())
-
-	configureCgroups()
-
-	syscall.Sethostname([]byte("container"))
-
-	syscall.Chroot("../images/ubuntu")
-	os.Chdir("/")
-
-	must(mountProc(""))
-
-	cmd := exec.Command(command, params...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	must(cmd.Run())
-	must(syscall.Unmount("proc", 0))
-
+	util.Must(cmd.Run())
 }
 
 func mountProc(newroot string) error {
@@ -79,20 +106,19 @@ func mountProc(newroot string) error {
 	return syscall.Mount(source, target, fstype, uintptr(flags), data)
 }
 
+func unmountProc(newroot string) error {
+	target := filepath.Join(newroot, "/proc")
+	flags := 0
+	return syscall.Unmount(target, flags)
+}
+
 func configureCgroups() {
 	cgroups := "/sys/fs/cgroup/"
 	pids := filepath.Join(cgroups, "pids")
 	os.Mkdir(filepath.Join(pids, "vini"), 0755)
 	//fmt.Println(filepath.Join(pids, "vini"))
-	must(ioutil.WriteFile(filepath.Join(pids, "vini/pids.max"), []byte("24"), 0700))
+	util.Must(ioutil.WriteFile(filepath.Join(pids, "vini/pids.max"), []byte("24"), 0700))
 	// Removes the new cgroup in place after the container exits
-	must(ioutil.WriteFile(filepath.Join(pids, "vini/notify_on_release"), []byte("1"), 0700))
-	must(ioutil.WriteFile(filepath.Join(pids, "vini/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
-}
-
-func must(err error) {
-	if err != nil {
-		fmt.Println("ERROR", err)
-		os.Exit(1)
-	}
+	util.Must(ioutil.WriteFile(filepath.Join(pids, "vini/notify_on_release"), []byte("1"), 0700))
+	util.Must(ioutil.WriteFile(filepath.Join(pids, "vini/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
 }
--- src/container/go.mod
+++ src/container/go.mod
@@ -1,1 +1,3 @@
 module github.com/viniciusbds/navio/src/container
+
+replace github.com/viniciusbds/navio/src/util => ../util
