ea83bbc29cb16c006dd44e94b490dc154ca80599
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-04-27 14:07:54 -0300

    Update [cmd]: update --help message of the version command

--- cmd/version.go
+++ cmd/version.go
@@ -6,15 +6,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const version = "0.0.1"
+
 func init() {
 	rootCmd.AddCommand(versionCmd)
 }
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version of Navio",
+	Short: "Show the Navio version",
 	Long:  "All software has versions. This is Navio's",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Navio 0.1")
+		fmt.Println("|___/ Navio ", version)
 	},
 }
