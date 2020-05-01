21191d98eeadfadfcabf28fc337f0995a44252b5
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-05-01 14:42:49 -0300

    Update [images]: improving the image package

---
+++ images/image.go
@@ -0,0 +1,106 @@
+package images
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/mgutz/ansi"
+	"github.com/viniciusbds/navio/logger"
+	"github.com/viniciusbds/navio/utilities"
+)
+
+var (
+	l       = logger.New(time.Kitchen, true)
+	magenta = ansi.ColorFunc("magenta+")
+)
+
+// Pull ...
+// [TODO]: Document this function
+func Pull(imageName string) {
+	image := getImage(imageName)
+
+	if image == nil {
+		msg := fmt.Sprintf("The image %s is not available", imageName)
+		l.Log("WARNING", msg)
+		return
+	}
+
+	if WasDownloaded(imageName) {
+		msg := fmt.Sprintf("The image %s already was downloaded", imageName)
+		l.Log("WARNING", msg)
+		return
+	}
+
+	l.Log("INFO", fmt.Sprintf("Pulling %s  from %s ...", imageName, image.url))
+
+	imagePath := "./images/" + imageName
+
+	if err := utilities.Wget(image.url, imageName+".tar"); err != nil {
+		l.Log("ERROR", fmt.Sprintf("The image %s was not Pulled", imageName))
+	}
+
+	if err := os.Mkdir(imagePath, 0777); err != nil {
+		l.Log("ERROR", fmt.Sprintf("The directory %s was not created", imagePath))
+	}
+
+	if err := utilities.Tar(imagePath, imageName+".tar"); err != nil {
+		l.Log("ERROR", fmt.Sprintf("The file %s was not extracted", imageName+".tar"))
+	}
+
+	if err := os.Remove(imageName + ".tar"); err != nil {
+		l.Log("ERROR", fmt.Sprintf("The file %s was not removed", imageName))
+	}
+
+	l.Log("INFO", "Pulled successfully :)\n")
+}
+
+// WasDownloaded ...
+// [TODO]: Document this function
+func WasDownloaded(imageName string) bool {
+	if _, err := os.Stat("./images/" + imageName); !os.IsNotExist(err) {
+		return true
+	}
+	return false
+}
+
+// ShowDownloadedImages ...
+// [TODO]: Document this function
+func ShowDownloadedImages() {
+	dirname := "./images"
+
+	f, err := os.Open(dirname)
+	if err != nil {
+		l.Log("ERROR", err.Error())
+	}
+
+	files, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		l.Log("ERROR", err.Error())
+	}
+
+	var imageStr string
+	fmt.Printf("NAME\t\tVERSION\t\tSIZE\n")
+	for _, file := range files {
+		if file.IsDir() {
+			imageStr = getImage(file.Name()).ToStr()
+			fmt.Println(magenta(imageStr))
+		}
+	}
+}
+
+// RemoveDownloadedImage ...
+// [TODO]: Document this function
+func RemoveDownloadedImage(imageName string) {
+	if WasDownloaded(imageName) {
+		err := os.RemoveAll("./images/" + imageName)
+		if err != nil {
+			l.Log("ERROR", err.Error())
+		} else {
+			l.Log("INFO", fmt.Sprintf("The image %s was removed sucessfully!", imageName))
+		}
+	} else {
+		l.Log("WARNING", fmt.Sprintf("The image %s doesn't exist.", imageName))
+	}
+}
--- images/images.go
+++ images/images.go
@@ -1,111 +1,43 @@
 package images
 
-import (
-	"fmt"
-	"os"
-	"time"
-
-	"github.com/mgutz/ansi"
-	"github.com/viniciusbds/navio/logger"
-	"github.com/viniciusbds/navio/utilities"
-)
+import "fmt"
 
 var (
-	l       = logger.New(time.Kitchen, true)
-	magenta = ansi.ColorFunc("magenta+")
+	alpine  = NewImage("alpine", "v3.11", "2.7M", "http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/x86_64/alpine-minirootfs-3.11.6-x86_64.tar.gz")
+	busybox = NewImage("busybox", "v4.0", "1.5M", "https://raw.githubusercontent.com/teddyking/ns-process/4.0/assets/busybox.tar")
+	ubuntu  = NewImage("ubuntu", "v20.04", "90.0M", "http://cloud-images.ubuntu.com/minimal/releases/focal/release/ubuntu-20.04-minimal-cloudimg-amd64-root.tar.xz")
 
-	// URL's
-	alpineURL  = "http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/x86_64/alpine-minirootfs-3.11.6-x86_64.tar.gz"
-	busyboxURL = "https://raw.githubusercontent.com/teddyking/ns-process/4.0/assets/busybox.tar"
-	ubuntuURL  = "http://cloud-images.ubuntu.com/minimal/releases/focal/release/ubuntu-20.04-minimal-cloudimg-amd64-root.tar.xz"
+	availableImages = []*Image{alpine, busybox, ubuntu}
 )
 
-// Pull ...
-// [TODO]: Document this function
-func Pull(imageName string) {
-
-	if CheckIfImageExists(imageName) {
-		msg := fmt.Sprintf("The image %s already was downloaded", imageName)
-		l.Log("WARNING", msg)
-		return
-	}
-
-	var imageURL string
-	switch imageName {
-	case "alpine":
-		imageURL = alpineURL
-	case "busybox":
-		imageURL = busyboxURL
-	case "ubuntu":
-		imageURL = ubuntuURL
-	}
-
-	l.Log("INFO", fmt.Sprintf("Pulling %s  from %s ...", imageName, imageURL))
-
-	imagePath := "./images/" + imageName
-
-	if err := utilities.Wget(imageURL, imageName+".tar"); err != nil {
-		l.Log("ERROR", fmt.Sprintf("The image %s was not Pulled", imageName))
-	}
-
-	if err := os.Mkdir(imagePath, 0777); err != nil {
-		l.Log("ERROR", fmt.Sprintf("The directory %s was not created", imagePath))
-	}
-
-	if err := utilities.Tar(imagePath, imageName+".tar"); err != nil {
-		l.Log("ERROR", fmt.Sprintf("The file %s was not extracted", imageName+".tar"))
-	}
-
-	if err := os.Remove(imageName + ".tar"); err != nil {
-		l.Log("ERROR", fmt.Sprintf("The file %s was not removed", imageName))
-	}
-
-	l.Log("INFO", "Pulled successfully :)\n")
+// Image ...
+type Image struct {
+	name    string
+	version string
+	size    string
+	url     string
 }
 
-// CheckIfImageExists ...
-// [TODO]: Document this function
-func CheckIfImageExists(imageName string) bool {
-	if _, err := os.Stat("./images/" + imageName); !os.IsNotExist(err) {
-		return true
+// NewImage ...
+func NewImage(name string, version string, size string, url string) *Image {
+	return &Image{
+		name:    name,
+		version: version,
+		size:    size,
+		url:     url,
 	}
-	return false
 }
 
-// ShowDownloadedImages ...
-// [TODO]: Document this function
-func ShowDownloadedImages() {
-	dirname := "./images"
+// ToStr ...
+func (image *Image) ToStr() string {
+	return fmt.Sprintf("%s\t\t%s\t\t%s", image.name, image.version, image.size)
+}
 
-	f, err := os.Open(dirname)
-	if err != nil {
-		l.Log("ERROR", err.Error())
-	}
-
-	files, err := f.Readdir(-1)
-	f.Close()
-	if err != nil {
-		l.Log("ERROR", err.Error())
-	}
-
-	for _, file := range files {
-		if file.IsDir() {
-			fmt.Println(magenta(file.Name()))
+func getImage(imageName string) *Image {
+	for _, i := range availableImages {
+		if i.name == imageName {
+			return i
 		}
 	}
-}
-
-// RemoveDownloadedImage ...
-// [TODO]: Document this function
-func RemoveDownloadedImage(image string) {
-	if CheckIfImageExists(image) {
-		err := os.RemoveAll("./images/" + image)
-		if err != nil {
-			l.Log("ERROR", err.Error())
-		} else {
-			l.Log("INFO", fmt.Sprintf("The image %s was removed sucessfully!", image))
-		}
-	} else {
-		l.Log("WARNING", fmt.Sprintf("The image %s doesn't exist.", image))
-	}
+	return nil
 }
