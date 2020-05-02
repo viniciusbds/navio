8b43a3788a62bae20cc391cf65acc6ff891b9ff8
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-05-02 17:26:53 -0300

    Test [logger] add logger 'testes'

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,1 +1,13 @@
 package logger
+
+import (
+	"testing"
+	"time"
+)
+
+func TestLog(t *testing.T) {
+	l := New(time.Kitchen, true)
+	l.Log("INFO", "This is a info message")
+	l.Log("wARNING", "This is a warning message")
+	l.Log("ERROR", "This is a error message")
+}
