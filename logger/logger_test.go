9438da27ef2d7e7395c70d17440f377119c40f88
Vinicius Barbosa <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-05-02 17:27:21 -0300

    Update logger_test.go

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -8,6 +8,6 @@ import (
 func TestLog(t *testing.T) {
 	l := New(time.Kitchen, true)
 	l.Log("INFO", "This is a info message")
-	l.Log("wARNING", "This is a warning message")
+	l.Log("WARNING", "This is a warning message")
 	l.Log("ERROR", "This is a error message")
 }
