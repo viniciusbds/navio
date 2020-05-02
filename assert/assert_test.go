6c8cac7ad2c20a21738acc07de6c656c707f21ef
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-05-02 11:45:32 -0300

    Test [assert] add new tests for TestImageisNotEmpty function

--- assert/assert_test.go
+++ assert/assert_test.go
@@ -1,1 +1,14 @@
 package assert
+
+import "testing"
+
+func TestImageisNotEmpty(t *testing.T) {
+	emptyImage := ""
+	nonemptyImage := "alpinex"
+	if err := ImageisNotEmpty(emptyImage); err.Error() != "The imageName must be a non-empty value" {
+		t.Errorf(err.Error())
+	}
+	if err := ImageisNotEmpty(nonemptyImage); err != nil {
+		t.Errorf(err.Error())
+	}
+}
