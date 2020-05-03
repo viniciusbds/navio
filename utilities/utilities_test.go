5b01524c2038a220a01bbf1d28555182c22e4918
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-05-03 09:13:16 -0300

    Add [utilities] tests

--- utilities/utilities_test.go
+++ utilities/utilities_test.go
@@ -1,1 +1,14 @@
 package utilities
+
+import "testing"
+
+func TestContains(t *testing.T) {
+	imageNames := []string{"alpine", "busybox", "ubuntu", "windows"}
+	if !Contains(imageNames, "alpine") || !Contains(imageNames, "busybox") ||
+		!Contains(imageNames, "ubuntu") || !Contains(imageNames, "windows") {
+		t.Error("Error on Contains function")
+	}
+	if Contains(imageNames, "kali") || Contains(imageNames, "") {
+		t.Error("Error on Contains function")
+	}
+}
