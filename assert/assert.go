59230e8beaa9bf288aa30f1600d51fb4f33554f6
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-05-03 11:08:45 -0300

    Update [asserts] refactore the ImageisNotEmpty function with TDD methodology

--- assert/assert.go
+++ assert/assert.go
@@ -2,6 +2,7 @@ package assert
 
 import (
 	"errors"
+	"strings"
 	"time"
 
 	"github.com/viniciusbds/navio/logger"
@@ -11,7 +12,7 @@ var l = logger.New(time.Kitchen, true)
 
 // ImageisNotEmpty ...
 func ImageisNotEmpty(imageName string) error {
-	if imageName == "" {
+	if len(strings.TrimSpace(imageName)) == 0 {
 		err := errors.New("The imageName must be a non-empty value")
 		l.Log("ERROR", err.Error())
 		return err
--- assert/assert_test.go
+++ assert/assert_test.go
@@ -3,12 +3,21 @@ package assert
 import "testing"
 
 func TestImageisNotEmpty(t *testing.T) {
-	emptyImage := ""
-	nonemptyImage := "alpinex"
-	if err := ImageisNotEmpty(emptyImage); err.Error() != "The imageName must be a non-empty value" {
-		t.Errorf(err.Error())
-	}
-	if err := ImageisNotEmpty(nonemptyImage); err != nil {
-		t.Errorf(err.Error())
-	}
+
+	t.Run("Simple case", func(t *testing.T) {
+		emptyImage := ""
+		nonemptyImage := "alpinex"
+		if err := ImageisNotEmpty(emptyImage); err.Error() != "The imageName must be a non-empty value" {
+			t.Errorf(err.Error())
+		}
+		if err := ImageisNotEmpty(nonemptyImage); err != nil {
+			t.Errorf(err.Error())
+		}
+	})
+
+	t.Run("Empty string with spaces", func(t *testing.T) {
+		if err := ImageisNotEmpty("    "); err.Error() != "The imageName must be a non-empty value" {
+			t.Errorf(err.Error())
+		}
+	})
 }
