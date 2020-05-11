9e93f166be28cb3946c2fdc49674436c0bb60e1e
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-05-11 17:09:05 -0300

    Add [utilities] define the paths where: we download all .tar files and the path where we untar it

--- utilities/constants.go
+++ utilities/constants.go
@@ -1,4 +1,10 @@
 package utilities
 
 // ImagesRootDir This is the default directory where we manipulate all images
-var ImagesRootDir = "/tmp"
+var ImagesRootDir = "/tmp/navioimages"
+
+// TarsPath is de directory where we store all image.tar files
+var TarsPath = ImagesRootDir + "/tars"
+
+// ImagesPath is de directory where we store all Images directories
+var ImagesPath = ImagesRootDir + "/images"
