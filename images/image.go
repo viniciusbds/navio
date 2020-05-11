f50605fa6ba08837faa775b6a175e69a69896e30
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-05-11 18:47:32 -0300

    Update [images] Dividindo o metodo pull em metodos menores

--- images/image.go
+++ images/image.go
@@ -20,83 +20,95 @@ var (
 	magenta = ansi.ColorFunc("magenta+")
 )
 
-// Pull ...
-// [TODO]: Document this function
+// Pull Downloads the .tar file from the official site
 func Pull(imageName string) error {
 	image := getImage(imageName)
 
 	if image == nil {
-		err := errors.New("The image " + imageName + " is not available")
-		l.Log("WARNING", err.Error())
+		msg := "Invalid image: " + image.name
+		l.Log("WARNING", msg)
+		return errors.New(msg)
+	}
+
+	if TarImageExists(image.name) {
+		l.Log("WARNING", "The image "+image.name+" already was downloaded")
+		return nil
+	}
+
+	l.Log("INFO", fmt.Sprintf("Pulling %s  from %s ...", image.name, image.url))
+
+	dir, _ := os.Getwd()
+	if tarsPathExists := utilities.FileExists(utilities.TarsPath); !tarsPathExists {
+		utilities.Must(os.MkdirAll(utilities.TarsPath, 0777))
+	}
+	utilities.Must(os.Chdir(utilities.TarsPath))
+	err := utilities.Wget(image.url, image.name+".tar")
+	if err != nil {
+		l.Log("ERROR", fmt.Sprintf("The image %s was not Pulled\n %s", image.name, err.Error()))
 		return err
 	}
-
-	if AlreadyExists(imageName) {
-		err := errors.New("The image " + imageName + " already was downloaded")
-		l.Log("WARNING", err.Error())
-		return err
-	}
-
-	l.Log("INFO", fmt.Sprintf("Pulling %s  from %s ...", imageName, image.url))
-
-	imagePath := utilities.ImagesRootDir + "/images/" + imageName
-
-	if err := utilities.Wget(image.url, imageName+".tar"); err != nil {
-		l.Log("ERROR", fmt.Sprintf("The image %s was not Pulled", imageName))
-		l.Log("ERROR", fmt.Sprintf("%s", err.Error()))
-		return err
-	}
-
-	if err := os.MkdirAll(imagePath, 0777); err != nil {
-		l.Log("ERROR", fmt.Sprintf("The directory %s was not created", imagePath))
-		l.Log("ERROR", fmt.Sprintf("%s", err.Error()))
-		return err
-	}
-
-	if err := utilities.Tar(imagePath, imageName+".tar"); err != nil {
-		l.Log("ERROR", fmt.Sprintf("The file %s was not extracted", imageName+".tar"))
-		l.Log("ERROR", fmt.Sprintf("%s", err.Error()))
-		return err
-	}
-
-	if err := os.Remove(imageName + ".tar"); err != nil {
-		l.Log("ERROR", fmt.Sprintf("The file %s was not removed", imageName))
-		l.Log("ERROR", fmt.Sprintf("%s", err.Error()))
-		return err
-	}
-
-	if imageName == "ubuntu" {
-		resolveFile := filepath.Join(imagePath, "/run/systemd/resolve/stub-resolv.conf")
-		if _, err := os.Stat(resolveFile); os.IsNotExist(err) {
-			utilities.Must(os.MkdirAll(imagePath+"/run/systemd/resolve", 0777))
-			//add a known DNS server to your system
-			utilities.Must(ioutil.WriteFile(resolveFile, []byte("nameserver 8.8.8.8\n"), 0644))
-
-			// see for more details: https://askubuntu.com/questions/91543/apt-get-update-fails-to-fetch-files-temporary-failure-resolving-error
-		}
-	}
+	utilities.Must(os.Chdir(dir))
 
 	l.Log("INFO", "Pulled successfully :)\n")
 	return nil
 }
 
-// AlreadyExists ...
-// [TODO]: Document this function
-func AlreadyExists(imageName string) bool {
-	if _, err := os.Stat(utilities.ImagesRootDir + "/images/" + imageName); !os.IsNotExist(err) {
-		return true
+// Prepare receive as argument the imageName and the containerName, create a directory with the
+// containerName and untar the respective image to this directory
+func Prepare(imageName, containerName string) error {
+	imagePath := filepath.Join(utilities.ImagesPath, containerName)
+	tarFile := filepath.Join(utilities.TarsPath, imageName) + ".tar"
+	if err := os.MkdirAll(imagePath, 0777); err != nil {
+		l.Log("ERROR", fmt.Sprintf("The directory %s was not created \n%s", imagePath, err.Error()))
+		return err
 	}
-	return false
+	if err := utilities.Untar(imagePath, tarFile); err != nil {
+		l.Log("ERROR", fmt.Sprintf("The image %s was not extracted. \n%s", imageName, err.Error()))
+		return err
+	}
+	// fmt.Printf("before insert %v\n", availableImages)
+	// availableImages = InsertANewImage(containerName, imageName, availableImages)
+	// fmt.Printf("after insert %v\n", availableImages)
+
+	return nil
+}
+
+// ConfigureNetworkForUbuntu Add the run/systemd/resolve/stub-resolv.conf file with the value "nameserver 8.8.8.8"
+// see for more details: https://askubuntu.com/questions/91543/apt-get-update-fails-to-fetch-files-temporary-failure-resolving-error
+func ConfigureNetworkForUbuntu(containerName string) {
+	imagePath := filepath.Join(utilities.ImagesPath, containerName)
+	resolveFile := filepath.Join(imagePath, "/run/systemd/resolve/stub-resolv.conf")
+	if _, err := os.Stat(resolveFile); os.IsNotExist(err) {
+		utilities.Must(os.MkdirAll(imagePath+"/run/systemd/resolve", 0777))
+		//add a known DNS server to your system
+		utilities.Must(ioutil.WriteFile(resolveFile, []byte("nameserver 8.8.8.8\n"), 0644))
+	}
+}
+
+// ImageIsReady receive a containerName as argument and return TRUE if her rootfs is ready
+// (i.e.: verify if there is a containerName dir on the ImagesPath directory)
+func ImageIsReady(containerName string) bool {
+	if _, err := os.Stat(filepath.Join(utilities.ImagesPath, containerName)); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+// TarImageExists receive a imageName as argument and return TRUE if the imageName.tar file exists
+// on the default TarsPath directory (see it on utilities.contants)
+func TarImageExists(imageName string) bool {
+	if _, err := os.Stat(filepath.Join(utilities.TarsPath, imageName) + ".tar"); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
 
 // ShowDownloadedImages ...
 // [TODO]: Document this function
 func ShowDownloadedImages() (string, error) {
-	dirname := utilities.ImagesRootDir + "/images"
-
-	f, err := os.Open(dirname)
+	f, err := os.Open(utilities.ImagesPath)
 	if err != nil {
-		os.Mkdir(dirname, 0777)
+		os.Mkdir(utilities.ImagesPath, 0777)
 		return "", nil
 	}
 
@@ -111,28 +123,28 @@ func ShowDownloadedImages() (string, error) {
 	result := ""
 	for _, file := range files {
 		if file.IsDir() {
-			imageStr = getImage(file.Name()).ToStr()
+			imageStr = file.Name() //getImage(file.Name()).ToStr()
 			result += "\n" + magenta(imageStr)
 		}
 	}
 	return result, nil
 }
 
-// RemoveDownloadedImage ...
+// DeleteImage ...
 // [TODO]: Document this function
-func RemoveDownloadedImage(imageName string) error {
-	if err := assert.ImageisNotEmpty(imageName); err != nil {
+func DeleteImage(containerName string) error {
+	if err := assert.ImageisNotEmpty(containerName); err != nil {
 		return err
 	}
-	if AlreadyExists(imageName) {
-		err := os.RemoveAll(utilities.ImagesRootDir + "/images/" + imageName)
+	if ImageIsReady(containerName) {
+		err := os.RemoveAll(filepath.Join(utilities.ImagesPath, containerName))
 		if err != nil {
 			l.Log("ERROR", err.Error())
 			return err
 		}
-		l.Log("INFO", fmt.Sprintf("The image %s was removed sucessfully!", imageName))
+		l.Log("INFO", fmt.Sprintf("The image %s was removed sucessfully!", containerName))
 	} else {
-		l.Log("WARNING", fmt.Sprintf("The image %s doesn't exist.", imageName))
+		l.Log("WARNING", fmt.Sprintf("The image %s doesn't exist.", containerName))
 	}
 	return nil
 }
--- images/images.go
+++ images/images.go
@@ -1,27 +1,32 @@
 package images
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var (
+	// AvailableImages ...
 	availableImages = []*Image{
-		NewImage("alpine", "v3.11", "2.7M", "http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/x86_64/alpine-minirootfs-3.11.6-x86_64.tar.gz"),
-		NewImage("busybox", "v4.0", "1.5M", "https://raw.githubusercontent.com/teddyking/ns-process/4.0/assets/busybox.tar"),
-		NewImage("ubuntu", "v20.04", "90.0M", "http://cloud-images.ubuntu.com/minimal/releases/focal/release/ubuntu-20.04-minimal-cloudimg-amd64-root.tar.xz"),
+		NewImage("alpine", "alpine", "v3.11", "2.7M", "http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/x86_64/alpine-minirootfs-3.11.6-x86_64.tar.gz"),
+		NewImage("busybox", "busybox", "v4.0", "1.5M", "https://raw.githubusercontent.com/teddyking/ns-process/4.0/assets/busybox.tar"),
+		NewImage("ubuntu", "ubuntu", "v20.04", "90.0M", "http://cloud-images.ubuntu.com/minimal/releases/focal/release/ubuntu-20.04-minimal-cloudimg-amd64-root.tar.xz"),
 	}
 )
 
 // Image ...
 type Image struct {
 	name    string
+	base    string
 	version string
 	size    string
 	url     string
 }
 
 // NewImage ...
-func NewImage(name string, version string, size string, url string) *Image {
+func NewImage(name string, base string, version string, size string, url string) *Image {
 	return &Image{
 		name:    name,
+		base:    base,
 		version: version,
 		size:    size,
 		url:     url,
@@ -41,3 +46,20 @@ func getImage(name string) *Image {
 	}
 	return nil
 }
+
+// IsValidImage ...
+func IsValidImage(name string) bool {
+	if getImage(name) != nil {
+		return true
+	}
+	return false
+}
+
+// // InsertANewImage ...
+// func InsertANewImage(containerName, imageName string, images []*Image) []*Image {
+// 	baseImage := getImage(imageName)
+// 	image := NewImage(containerName, imageName, baseImage.version, baseImage.size, baseImage.url)
+// 	availableImages = append(images, image)
+// 	fmt.Printf("insert %v\n", images)
+// 	return availableImages
+// }
