ec8b441323a25364c04dbefcc4e78404abd03a70
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-05-13 19:16:37 -0300

    Add [cmd] add a new command: navio ps

---
+++ cmd/ps.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/viniciusbds/navio/images"
+	"github.com/viniciusbds/navio/utilities"
+)
+
+func init() {
+	rootCmd.AddCommand(ps())
+}
+
+func ps() *cobra.Command {
+	return &cobra.Command{
+		Use:   "ps",
+		Short: "Shows all containerImages that was created",
+		Long:  "Each of thesees containerImages are a /rootf of the respective container that was created",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Println("NAME\t\t\tBASE\t\tVERSION\t\tSIZE")
+			imageList, _ := images.Ps()
+			if !utilities.IsEmpty(imageList) {
+				fmt.Println(imageList)
+			}
+
+			return nil
+		},
+	}
+}
