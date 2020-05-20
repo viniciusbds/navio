cd0b3e6d47b8089c1e2af72fc276a51ce3bcc239
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-05-20 20:46:26 -0300

    Simplificando o pacote de imagens.

--- images/image.go
+++ images/image.go
@@ -2,183 +2,70 @@ package images
 
 import (
 	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"time"
-
-	"errors"
-
-	"github.com/mgutz/ansi"
-	"github.com/viniciusbds/navio/assert"
-	"github.com/viniciusbds/navio/logger"
-	"github.com/viniciusbds/navio/utilities"
 )
 
 var (
-	l       = logger.New(time.Kitchen, true)
-	magenta = ansi.ColorFunc("magenta+")
+	images = make(map[string]*Image)
 )
 
-// Pull Downloads the .tar file from the official site
-func Pull(imageName string) error {
-	if !utilities.IsOfficialImage(imageName) {
-		msg := fmt.Sprintf("%s is not a official Image. Select one of the: %v", imageName, utilities.OfficialImages)
-		l.Log("WARNING", msg)
-		return errors.New(msg)
-	}
-
-	image := getImage(imageName)
-
-	if TarImageExists(image.name) {
-		msg := "The image " + image.name + " already was downloaded"
-		l.Log("WARNING", msg)
-		return errors.New(msg)
-	}
-
-	l.Log("INFO", fmt.Sprintf("Pulling %s  from %s ...", image.name, image.url))
-
-	dir, _ := os.Getwd()
-	if tarsPathExists := utilities.FileExists(utilities.TarsPath); !tarsPathExists {
-		utilities.Must(os.MkdirAll(utilities.TarsPath, 0777))
-	}
-	utilities.Must(os.Chdir(utilities.TarsPath))
-	err := utilities.Wget(image.url, image.name+".tar")
-	if err != nil {
-		l.Log("ERROR", fmt.Sprintf("The image %s was not Pulled\n %s", image.name, err.Error()))
-		return err
-	}
-	utilities.Must(os.Chdir(dir))
-
-	l.Log("INFO", "Pulled successfully :)\n")
-	return nil
+func init() {
+	readImagesDB()
 }
 
-// Prepare receive as argument the baseImage and the containerName, create a directory with the
-// containerName and untar the respective image to this directory
-func Prepare(baseImage, containerName string) error {
-	imagePath := filepath.Join(utilities.ImagesPath, containerName)
-	tarFile := filepath.Join(utilities.TarsPath, baseImage) + ".tar"
-	if err := os.MkdirAll(imagePath, 0777); err != nil {
-		l.Log("ERROR", fmt.Sprintf("The directory %s was not created \n%s", imagePath, err.Error()))
-		return err
-	}
-	if err := utilities.Untar(imagePath, tarFile); err != nil {
-		l.Log("ERROR", fmt.Sprintf("The image %s was not extracted. \n%s", baseImage, err.Error()))
-		return err
-	}
-	InsertContImage(containerName, baseImage)
-	return nil
+// Image ...
+type Image struct {
+	name    string
+	base    string
+	version string
+	size    string
+	url     string
 }
 
-// ConfigureNetworkForUbuntu Add the run/systemd/resolve/stub-resolv.conf file with the value "nameserver 8.8.8.8"
-// see for more details: https://askubuntu.com/questions/91543/apt-get-update-fails-to-fetch-files-temporary-failure-resolving-error
-func ConfigureNetworkForUbuntu(containerName string) {
-	imagePath := filepath.Join(utilities.ImagesPath, containerName)
-	resolveFile := filepath.Join(imagePath, "/run/systemd/resolve/stub-resolv.conf")
-	if _, err := os.Stat(resolveFile); os.IsNotExist(err) {
-		utilities.Must(os.MkdirAll(imagePath+"/run/systemd/resolve", 0777))
-		//add a known DNS server to your system
-		utilities.Must(ioutil.WriteFile(resolveFile, []byte("nameserver 8.8.8.8\n"), 0644))
+// NewImage ...
+func NewImage(name string, base string, version string, size string, url string) *Image {
+	return &Image{
+		name:    name,
+		base:    base,
+		version: version,
+		size:    size,
+		url:     url,
 	}
 }
 
-// IsContImageReady receive a containerName as argument and return TRUE if her rootfs is ready
-// (i.e.: verify if there is a containerName dir on the ImagesPath directory)
-func IsContImageReady(containerName string) bool {
-	if _, err := os.Stat(filepath.Join(utilities.ImagesPath, containerName)); os.IsNotExist(err) {
-		return false
-	}
-	return true
-}
-
-// TarImageExists receive a imageName as argument and return TRUE if the imageName.tar file exists
-// on the default TarsPath directory (see it on utilities.contants)
-func TarImageExists(imageName string) bool {
-	if _, err := os.Stat(filepath.Join(utilities.TarsPath, imageName) + ".tar"); os.IsNotExist(err) {
-		return false
-	}
-	return true
-}
-
-// ShowBaseImages return a string with all base images
-func ShowBaseImages() (string, error) {
-	result := ""
-	for _, img := range baseImages {
-		result += "\n" + magenta(img.ToStr())
-	}
-	return result, nil
-}
-
-// Ps return a string with all availables container images that was created. see it like "containers"
-func Ps() (string, error) {
-	result := ""
-	for _, img := range contImages {
-		result += "\n" + magenta(img.ToStr())
-	}
-	return result, nil
-}
-
-// DeleteContImage receives a containerImage and remove it
-func DeleteContImage(containerName string) {
-	if err := assert.ImageisNotEmpty(containerName); err != nil {
-		l.Log("WARNING", "Cannot remove a empty image: "+containerName)
-		return
-	}
-	if IsContImageReady(containerName) {
-		if err := os.RemoveAll(filepath.Join(utilities.ImagesPath, containerName)); err != nil {
-			l.Log("ERROR", err.Error())
-			return
-		}
-		l.Log("INFO", fmt.Sprintf("The image %s was removed sucessfully!", containerName))
+// ToStr ... Ignore esse código, essa foi a minha maior vigarice (https://www.youtube.com/watch?v=PK0c_n5EDhk)
+func (i *Image) ToStr() string {
+	tab := ""
+	if len(i.name) < 8 {
+		tab = "\t\t\t\t\t"
+	} else if len(i.name) >= 8 && len(i.name) < 16 {
+		tab = "\t\t\t\t"
 	} else {
-		l.Log("WARNING", fmt.Sprintf("The image %s doesn't exist.", containerName))
+		tab = "\t\t\t"
 	}
-	removeContImage(containerName)
+	return fmt.Sprintf("%s%s%s\t\t\t%s\t\t\t%s", i.name, tab, i.base, i.version, i.size)
 }
 
-// DeleteBaseImage ...
-func DeleteBaseImage(baseImage string) {
-	if utilities.IsOfficialImage(baseImage) {
-		l.Log("WARNING", "Cannot remove a official image")
-		return
+func getImage(name string) *Image {
+	if images[name] != nil {
+		return images[name]
 	}
-	if err := assert.ImageisNotEmpty(baseImage); err != nil {
-		l.Log("WARNING", "Cannot remove a empty image: "+baseImage)
-		return
-	}
-	if TarImageExists(baseImage) {
-		if err := os.RemoveAll(filepath.Join(utilities.TarsPath, baseImage+".tar")); err != nil {
-			l.Log("ERROR", err.Error())
-			return
-		}
-		l.Log("INFO", fmt.Sprintf("The image %s was removed sucessfully!", baseImage))
-	} else {
-		l.Log("WARNING", fmt.Sprintf("The image %s doesn't exist.", baseImage))
-	}
-	removeBaseImage(baseImage)
-}
-
-// Describe ...
-func Describe(imageName string) string {
-	image := getImage(imageName)
-	if image == nil {
-		l.Log("WARNING", fmt.Sprintf("Invalid image! Cannot describe %s", imageName))
-		return ""
-	}
-	return getImage(imageName).ToStr()
-}
-
-// BuildANewBaseImg ...
-func BuildANewBaseImg(newImg, baseImg string) error {
-	newImgPath := filepath.Join(utilities.ImagesPath, newImg)
-	tarFile := filepath.Join(utilities.TarsPath, baseImg) + ".tar"
-	if err := os.Mkdir(newImgPath, 0777); err != nil {
-		return err
-	}
-	if err := utilities.Untar(newImgPath, tarFile); err != nil {
-		return err
-	}
-	InsertBaseImage(newImg, baseImg)
 	return nil
+}
+
+// InsertImage ...
+func InsertImage(name, baseImage string) {
+	baseImg := getImage(baseImage)
+	newImg := NewImage(name, baseImage, baseImg.version, baseImg.size, baseImg.url)
+	images[name] = newImg
+	insertImageDB(newImg)
+}
+
+// IsaBaseImage receive a imageName and return true if is a base image.
+func IsaBaseImage(image string) bool {
+	for _, i := range images {
+		if image == i.name {
+			return true
+		}
+	}
+	return false
 }
--- images/images.go
+++ images/images.go
@@ -1,217 +1,145 @@
 package images
 
 import (
-	"encoding/csv"
 	"fmt"
-	"io"
-	"log"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"time"
 
+	"errors"
+
+	"github.com/mgutz/ansi"
+	"github.com/viniciusbds/navio/logger"
 	"github.com/viniciusbds/navio/utilities"
 )
 
 var (
-	contImages = make(map[string]*Image)
-	baseImages = make(map[string]*Image)
+	l       = logger.New(time.Kitchen, true)
+	magenta = ansi.ColorFunc("magenta+")
 )
 
-func init() {
-
-	if utilities.FileExists(utilities.BaseImagescsv) {
-		readBaseImagesCSV()
-	} else {
-		baseImages["alpine"] = NewImage("alpine", "alpine", "v3.11", "2.7M", utilities.AlpineURL)
-		baseImages["busybox"] = NewImage("busybox", "busybox", "v4.0", "1.5M", utilities.BusyboxURL)
-		baseImages["ubuntu"] = NewImage("ubuntu", "ubuntu", "v20.04", "90.0M", utilities.Ubuntu20ltsURL)
-		updateBaseImagesCSV()
+// Pull Downloads the .tar file from the official site
+func Pull(imageName string) error {
+	if !utilities.IsOfficialImage(imageName) {
+		return errors.New(imageName + " is not a official Image.")
 	}
 
-	if utilities.FileExists(utilities.ContImagescsv) {
-		readContImagesCSV()
+	image := getImage(imageName)
+
+	if Exists(image.name) {
+		return errors.New("The image " + image.name + " already was downloaded")
 	}
+
+	l.Log("INFO", fmt.Sprintf("Pulling %s  from %s ...", image.name, image.url))
+
+	dir, _ := os.Getwd()
+	if tarsPathExists := utilities.FileExists(utilities.ImagesPath); !tarsPathExists {
+		utilities.Must(os.MkdirAll(utilities.ImagesPath, 0777))
+	}
+	utilities.Must(os.Chdir(utilities.ImagesPath))
+	err := utilities.Wget(image.url, image.name+".tar")
+	if err != nil {
+		l.Log("ERROR", fmt.Sprintf("The image %s was not Pulled\n %s", image.name, err.Error()))
+		return err
+	}
+	utilities.Must(os.Chdir(dir))
+
+	l.Log("INFO", "Pulled successfully :)\n")
+	return nil
 }
 
-// Image ...
-type Image struct {
-	name    string
-	base    string
-	version string
-	size    string
-	url     string
-}
-
-// NewImage ...
-func NewImage(name string, base string, version string, size string, url string) *Image {
-	return &Image{
-		name:    name,
-		base:    base,
-		version: version,
-		size:    size,
-		url:     url,
+// PrepareRootfs ...
+func PrepareRootfs(baseImage, containerName string) error {
+	rootfsPath := filepath.Join(utilities.RootfsPath, containerName)
+	tarFile := filepath.Join(utilities.ImagesPath, baseImage) + ".tar"
+	if err := os.MkdirAll(rootfsPath, 0777); err != nil {
+		return err
 	}
-}
-
-// ToStr ... Ignore esse código, essa foi a minha maior vigarice (https://www.youtube.com/watch?v=PK0c_n5EDhk)
-func (i *Image) ToStr() string {
-	tab := ""
-	if len(i.name) < 8 {
-		tab = "\t\t\t\t\t"
-	} else if len(i.name) >= 8 && len(i.name) < 16 {
-		tab = "\t\t\t\t"
-	} else {
-		tab = "\t\t\t"
-	}
-	return fmt.Sprintf("%s%s%s\t\t\t%s\t\t\t%s", i.name, tab, i.base, i.version, i.size)
-}
-
-func getImage(name string) *Image {
-	if contImages[name] != nil {
-		return contImages[name]
-	}
-	if baseImages[name] != nil {
-		return baseImages[name]
+	if err := utilities.Untar(rootfsPath, tarFile); err != nil {
+		return err
 	}
 	return nil
 }
 
-// IsValidContainerImgName verifies if a Container Image Name wasn't used.
-// If the containerImgName is available, return true.
-func IsValidContainerImgName(containerImgName string) bool {
-	if contImages[containerImgName] != nil {
-		return true
+// ConfigureNetworkForUbuntu Add the run/systemd/resolve/stub-resolv.conf file with the value "nameserver 8.8.8.8"
+// see for more details: https://askubuntu.com/questions/91543/apt-get-update-fails-to-fetch-files-temporary-failure-resolving-error
+func ConfigureNetworkForUbuntu(containerName string) {
+	rootfsPath := filepath.Join(utilities.RootfsPath, containerName)
+	resolveFile := filepath.Join(rootfsPath, "/run/systemd/resolve/stub-resolv.conf")
+	if _, err := os.Stat(resolveFile); os.IsNotExist(err) {
+		utilities.Must(os.MkdirAll(rootfsPath+"/run/systemd/resolve", 0777))
+		//add a known DNS server to your system
+		utilities.Must(ioutil.WriteFile(resolveFile, []byte("nameserver 8.8.8.8\n"), 0644))
 	}
-	return false
 }
 
-// InsertContImage receive a containerImgName and the respective baseImage and creates a new
-// Image and insert it on [contImages map]. Also update the csv file that store all containerImages
-func InsertContImage(containerImgName, baseImage string) {
-	baseImg := getImage(baseImage)
-	newImg := NewImage(containerImgName, baseImage, baseImg.version, baseImg.size, baseImg.url)
-	contImages[containerImgName] = newImg
-	updateContImagesCSV()
+// RootfsExists ...
+func RootfsExists(containerName string) bool {
+	if _, err := os.Stat(filepath.Join(utilities.RootfsPath, containerName)); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
 
-// InsertBaseImage ...
-func InsertBaseImage(newBaseImg, base string) {
-	baseImg := getImage(base)
-	newImg := NewImage(newBaseImg, base, baseImg.version, baseImg.size, baseImg.url)
-	baseImages[newBaseImg] = newImg
-	updateBaseImagesCSV()
+// Exists receive a imageName as argument and return TRUE if the imageName.tar file exists
+// on the default TarsPath directory (see it on utilities.contants)
+func Exists(image string) bool {
+	if _, err := os.Stat(filepath.Join(utilities.ImagesPath, image) + ".tar"); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
 
-func removeContImage(contImamge string) {
-	delete(contImages, contImamge)
-	updateContImagesCSV()
+// GetImages return a string with all base images
+func GetImages() (result string, err error) {
+	for _, img := range images {
+		result += "\n" + magenta(img.ToStr())
+	}
+	return result, nil
 }
 
-func removeBaseImage(baseImage string) {
-	delete(baseImages, baseImage)
-	updateBaseImagesCSV()
-}
-
-// IsaBaseImage receive a imageName and return true if is a base image.
-func IsaBaseImage(image string) bool {
-	for _, i := range baseImages {
-		if image == i.name {
-			return true
+// RemoveImage ...
+func RemoveImage(image string) error {
+	if utilities.IsOfficialImage(image) {
+		return errors.New("Cannot remove a official image")
+	}
+	if utilities.IsEmpty(image) {
+		return errors.New("Cannot remove a empty image")
+	}
+	if Exists(image) {
+		if err := os.RemoveAll(filepath.Join(utilities.ImagesPath, image+".tar")); err != nil {
+			return err
 		}
 	}
-	return false
+	deleteImage(image)
+	return nil
 }
 
-func readContImagesCSV() {
-	contImages = make(map[string]*Image)
-
-	csvfile, err := os.Open(utilities.ContImagescsv)
-	if err != nil {
-		l.Log("Error", err.Error())
-	}
-
-	r := csv.NewReader(csvfile)
-	for {
-		// Read each record from csv
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		contImages[record[0]] = NewImage(record[0], record[1], record[2], record[3], record[4])
-	}
+func deleteImage(imgName string) {
+	delete(images, imgName)
+	removeImageDB(imgName)
 }
 
-func updateContImagesCSV() {
-	err := os.RemoveAll(utilities.ContImagescsv)
-	if err != nil {
-		log.Fatalln("Couldn't remove the csv file", err)
+// Describe ...
+func Describe(imageName string) (string, error) {
+	image := getImage(imageName)
+	if image == nil {
+		return "", errors.New("Invalid image! Cannot describe" + imageName)
 	}
-
-	csvfile, err := os.Create(utilities.ContImagescsv)
-	if err != nil {
-		log.Fatalln("Couldn't create the csv file", err)
-	}
-
-	w := csv.NewWriter(csvfile)
-
-	for _, v := range contImages {
-		newimage := []string{v.name, v.base, v.version, v.size, v.url}
-		err = w.Write(newimage)
-		if err != nil {
-			fmt.Println("Erro aque")
-		}
-	}
-	w.Flush()
-	csvfile.Close()
+	return getImage(imageName).ToStr(), nil
 }
 
-func readBaseImagesCSV() {
-	baseImages = make(map[string]*Image)
-
-	csvfile, err := os.Open(utilities.BaseImagescsv)
-	if err != nil {
-		l.Log("Error", err.Error())
+// BuildANewBaseImg ...
+func BuildANewBaseImg(name, baseImg string) error {
+	newImgPath := filepath.Join(utilities.RootfsPath, name)
+	tarFile := filepath.Join(utilities.ImagesPath, baseImg) + ".tar"
+	if err := os.Mkdir(newImgPath, 0777); err != nil {
+		return err
 	}
-
-	r := csv.NewReader(csvfile)
-	for {
-		// Read each record from csv
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		baseImages[record[0]] = NewImage(record[0], record[1], record[2], record[3], record[4])
+	if err := utilities.Untar(newImgPath, tarFile); err != nil {
+		return err
 	}
-}
-
-func updateBaseImagesCSV() {
-	err := os.RemoveAll(utilities.BaseImagescsv)
-	if err != nil {
-		log.Fatalln("Couldn't remove the csv file", err)
-	}
-
-	if !utilities.FileExists(utilities.ImagesRootDir) {
-		os.MkdirAll(utilities.ImagesRootDir, 0777)
-	}
-
-	csvfile, err := os.Create(utilities.BaseImagescsv)
-	if err != nil {
-		log.Fatalln("Couldn't create the csv file [picuinha]", err)
-	}
-
-	w := csv.NewWriter(csvfile)
-
-	for _, v := range baseImages {
-		newimage := []string{v.name, v.base, v.version, v.size, v.url}
-		err = w.Write(newimage)
-		if err != nil {
-			fmt.Println("Erro aque")
-		}
-	}
-	w.Flush()
-	csvfile.Close()
+	InsertImage(name, baseImg)
+	return nil
 }
