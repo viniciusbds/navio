08931c8ae6f75fd9d69401f0a9ec23a56469cac5
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-05-23 09:48:30 -0300

    Update [utilities] remove unused constants and change the Root dir to persist the data

--- utilities/constants.go
+++ utilities/constants.go
@@ -1,32 +1,16 @@
-package utilities
+package constants
 
 // NavioVersion is the current version of Navio
-var NavioVersion = "v1.0"
+var NavioVersion = "v1.1"
 
-// ImagesRootDir  is the default directory where we manipulate all images
-var ImagesRootDir = "/tmp/navioimages"
+// RootDir is the place where we store all Navio's data
+var RootDir = "/usr/local/navio"
 
-// ImagesPath is the directory where we store all image.tar files
-var ImagesPath = ImagesRootDir + "/tars"
+// ImagesPath is the sub-directory where we store all image.tar files.
+var ImagesPath = RootDir + "/images"
 
-// RootfsPath is the directory where we store all Rootfs directories
-var RootfsPath = ImagesRootDir + "/images"
+// RootFSPath is the sub-directory where we store all Rootfs directories.
+var RootFSPath = RootDir + "/roots"
 
-// ContImagescsv is the file where we all container images (for example, if you create a container
-// with "ana" as name, the image "ana" will be here)
-var ContImagescsv = ImagesRootDir + "/contimages.csv"
-
-// BaseImagescsv ...
-var BaseImagescsv = ImagesRootDir + "/baseimages.csv"
-
-// OfficialImages are the official images that are currently suported
+// OfficialImages are the official images that are currently suported.
 var OfficialImages = []string{"alpine", "busybox", "ubuntu"}
-
-// AlpineURL ...
-var AlpineURL = "http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/x86_64/alpine-minirootfs-3.11.6-x86_64.tar.gz"
-
-// BusyboxURL ...
-var BusyboxURL = "https://raw.githubusercontent.com/teddyking/ns-process/4.0/assets/busybox.tar"
-
-// Ubuntu20ltsURL ...
-var Ubuntu20ltsURL = "http://cloud-images.ubuntu.com/minimal/releases/focal/release/ubuntu-20.04-minimal-cloudimg-amd64-root.tar.xz"
