7fecbc0f3a5c04ce33aa06c59a4af37783737f80
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-05-23 09:53:03 -0300

    Update [cleaning] the mysql database file

--- navioDB.sql
+++ navioDB.sql
@@ -4,23 +4,24 @@ USE  navio;
 -- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
 
 CREATE TABLE containers (
-  `id` varchar(30) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `imageid` varchar(30) NOT NULL,
-  `status` varchar(10) NOT NULL,
-  `root` varchar(30) NOT NULL,
-  `command` varchar(130) NOT NULL,
-  PRIMARY KEY (`id`)
+  id varchar(30) NOT NULL,
+  name varchar(30) NOT NULL,
+  image varchar(30) NOT NULL,
+  status varchar(10) NOT NULL,
+  root varchar(300) NOT NULL,
+  command varchar(300) NOT NULL,
+  params varchar(300),
+  PRIMARY KEY (id)
 )  DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE images (
-  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL,
-  `base` varchar(30) NOT NULL,
-  `version` varchar(10) NOT NULL,
-  `size` varchar(10) NOT NULL,
-  `url` varchar(130) NOT NULL,
+  id int(6) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(30) NOT NULL,
+  base varchar(30) NOT NULL,
+  version varchar(10) NOT NULL,
+  size varchar(10) NOT NULL,
+  url varchar(130) NOT NULL,
   PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
 
