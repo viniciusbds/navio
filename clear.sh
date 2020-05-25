1cbc2833314eb97aa3421cf979ed74c2451f9748
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-05-25 07:49:16 -0300

    Update [script] clear.sh to clear and re-open the database

--- clear.sh
+++ clear.sh
@@ -2,4 +2,14 @@
 
 # THIS SCRIPT CLEAR ALL Navio's data from the system
 # obs: this will not uninstall the Navio.
+
+
+# REMOVE NAVIO FILES (ROOTFS + IMAGES.tar's)
 sudo rm -r "/usr/local/navio"
+
+# Re-open (reset) the database
+mysqlversion=`mysql --version`
+goversion=`go version`
+if [ -n "$goversion"  ] && [ -n "$mysqlversion"  ]; then 
+    go run ./database/up.go
+fi
