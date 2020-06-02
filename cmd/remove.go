b4feaa0bbc4d9b4b33fbb8e9aea6520f6fc6d00d
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-06-02 16:52:43 -0300

    Refactore [cmd] remove functions

---
+++ cmd/remove.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/viniciusbds/navio/container"
+)
+
+func init() {
+	rootCmd.AddCommand(remove())
+}
+
+func remove() *cobra.Command {
+	return &cobra.Command{
+		Use:   "remove",
+		Short: "Remove one or more containers",
+		Run: func(cmd *cobra.Command, args []string) {
+
+			if len(args) == 0 {
+				l.Log("WARNING", "You must insert the containerName!")
+				return
+			}
+
+			for _, arg := range args {
+
+				if !container.Exists(arg) {
+					l.Log("WARNING", "The container "+arg+" doesn't exists!")
+					continue
+				}
+
+				err := container.RemoveContainer(arg)
+				if err != nil {
+					l.Log("ERROR", err.Error())
+				}
+			}
+
+		},
+	}
+}
