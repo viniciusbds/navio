9e6779f114005256d9eb34472ef4ab570fe3c48e
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-06-03 17:31:03 -0300

    Refactore [cmd] exec

--- cmd/exec.go
+++ cmd/exec.go
@@ -22,22 +22,18 @@ func exec() *cobra.Command {
 	return &cobra.Command{
 		Use:   "exec",
 		Short: "Run a command in a running container",
-		RunE: func(cmd *cobra.Command, args []string) error {
-
-			if containerName == "" && containerID == "" {
-				l.Log("WARNING", "You must insert a container name or a container id. (ex.: --name containerName or --id containerID)")
-				return nil
+		Run: func(cmd *cobra.Command, args []string) {
+			if utilities.IsEmpty(containerID) {
+				l.Log("WARNING", "You must insert a container id. (ex --id 9999987)")
+				return
+			}
+			if !utilities.IsEmpty(containerID) && !container.IsaID(containerID) {
+				l.Log("WARNING", "Invalid container id. Run [navio containers] to see the available ones.")
+				return
 			}
 			if len(args) < 1 {
 				l.Log("WARNING", "You must insert a command!")
-				return nil
-			}
-			if utilities.IsEmpty(containerID) {
-				containerID = container.GetContainerID(containerName)
-			}
-			if !container.Exists(containerID) {
-				l.Log("WARNING", fmt.Sprintf("%s is not a valid container. Run [navio containers] to see the available ones.", containerName))
-				return nil
+				return
 			}
 
 			command := args[0]
@@ -46,8 +42,6 @@ func exec() *cobra.Command {
 			l.Log("INFO", fmt.Sprintf("Container: %s, Command: %s, Params: %v", containerName, command, params))
 			args = append([]string{containerID, containerName, command}, params...)
 			container.Exec(args)
-
-			return nil
 		},
 	}
 }
