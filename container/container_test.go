0cd94d77b44e5eb03f5e185e80005958089e9577
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-06-05 14:37:19 -0300

    Fix  a unit test

--- container/container_test.go
+++ container/container_test.go
@@ -4,11 +4,11 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/viniciusbds/navio/utilities"
+	"github.com/viniciusbds/navio/constants"
 )
 
 func TestNewContainer(t *testing.T) {
-	cont := NewContainer("98907342", "conteiir", "ubuntu", "OK", filepath.Join(utilities.RootFSPath, "conteiir"), "echo", []string{"oi", "oi"})
+	cont := NewContainer("98907342", "conteiir", "ubuntu", "OK", filepath.Join(constants.RootFSPath, "conteiir"), "echo", []string{"oi", "oi"})
 	if cont.Name != "conteiir" || cont.Status != "OK" {
 		t.Errorf("Coisas estranhas aconteceram")
 	}
