9e36771e236e48e1748b76e9b8a9c04ab3f6f95c
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-06-06 10:15:17 -0300

    Add [container] create tests

--- container/create_test.go
+++ container/create_test.go
@@ -1,3 +1,25 @@
 package container
 
-// [TODO: test] CreateContainer creates a container based on a baseImg, containerName and command with params
+
+import (
+	"testing"
+)
+
+func TestCreateContainer(t *testing.T) {
+
+	id1, id2, id3 := GenerateNewID(), GenerateNewID(), GenerateNewID()
+	for _, ID := range []string{id1, id2, id3} {
+		AssertContainerDontExists(ID, t)
+	}
+
+	go CreateContainer(id1, "alpix", "alpine", "echo", []string{"alpix"}, done)
+	<-done
+	go CreateContainer(id2, "alpex", "alpine", "echo", []string{"alpex"}, done)
+	<-done
+	go CreateContainer(id3, "alpux", "alpine", "echo", []string{"alpux"}, done)
+	<-done
+
+	for _, ID := range []string{id1, id2, id3} {
+		AssertContainerExists(ID, t)
+	}
+}
