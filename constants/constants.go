720608946165e7ec31bb237b86da4b8147d52a14
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-07-28 19:10:10 -0300

    refactoring constants: using the correct keyword "const" instead of "var"

--- constants/constants.go
+++ constants/constants.go
@@ -1,31 +1,45 @@
 package constants
 
-// NavioVersion is the current version of Navio
-var NavioVersion = "v2.0"
+const (
+	// NavioVersion is the current version of Navio
+	NavioVersion = "v2.0"
 
-// RootDir is the place where we store all Navio's data
-var RootDir = "/usr/local/navio"
+	// RootDir is the place where we store all Navio's data
+	RootDir = "/usr/local/navio"
 
-// ImagesPath is the sub-directory where we store all image.tar files.
-var ImagesPath = RootDir + "/images"
+	// ImagesPath is the sub-directory where we store all image.tar files.
+	ImagesPath = RootDir + "/images"
 
-// RootFSPath is the sub-directory where we store all Rootfs directories.
-var RootFSPath = RootDir + "/roots"
+	// RootFSPath is the sub-directory where we store all Rootfs directories.
+	RootFSPath = RootDir + "/roots"
 
-// DBuser represents the database user
-var DBuser = "root"
+	// DBuser represents the database user
+	DBuser = "root"
 
-// DBpass represents the database password
-var DBpass = "root"
+	// DBpass represents the database password
+	DBpass = "root"
 
-// DBname represents the database name...
-var DBname = "navio"
+	// DBname represents the database name...
+	DBname = "navio"
 
-// MaxImageNameLength  23 == len("NAME                    ") -1
-var MaxImageNameLength = 23
+	// MaxImageNameLength  23 == len("NAME                    ") -1
+	MaxImageNameLength = 23
 
-// MaxContainerNameLength  23 == len("NAME                    ") -1
-var MaxContainerNameLength = 23
+	// MaxContainerNameLength  23 == len("NAME                    ") -1
+	MaxContainerNameLength = 23
+
+	// DefaultMaxProcessCreation ...
+	DefaultMaxProcessCreation = "30"
+
+	// DefaultCPUS ...
+	DefaultCPUS = "0"
+
+	// DefaultCPUshares ...
+	DefaultCPUshares = "1024"
+
+	// DefaultMemlimit ...
+	DefaultMemlimit = "1G"
+)
 
 // OfficialImages are the official images that are currently suported.
 var OfficialImages = []string{"alpine", "busybox", "ubuntu"}
@@ -39,15 +53,3 @@ func IsOfficialImage(image string) bool {
 	}
 	return false
 }
-
-// DefaultMaxProcessCreation ...
-var DefaultMaxProcessCreation string = "30"
-
-// DefaultCPUS ...
-var DefaultCPUS string = "0"
-
-// DefaultCPUshares ...
-var DefaultCPUshares string = "1024"
-
-// DefaultMemlimit ...
-var DefaultMemlimit string = "1G"
